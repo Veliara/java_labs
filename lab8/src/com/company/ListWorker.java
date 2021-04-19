00a960316ff152a5fb4ad2cef46f9284cb312609
Veliara <66844273+Veliara@users.noreply.github.com>
2021-04-19 12:15:40 +0300

    Update ListWorker.java

--- lab8/src/com/company/ListWorker.java
+++ lab8/src/com/company/ListWorker.java
@@ -1,7 +1,8 @@
 package com.company;
 
-import java.util.Comparator;
-import java.util.List;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
 public class ListWorker {
@@ -15,18 +16,29 @@ public class ListWorker {
         }
     }
 
+    // список покупців, упорядкований за зростанням балансу рахунку, а при
+    //рівності балансів – за номером кредитної картки
+    public List<Customer> listSortBalance(List<Customer> list) {
+        list.sort(Comparator.comparingDouble(Customer::getBalance)
+                .thenComparing(Customer::getCreditCardNum));
+        return list;
+    }
+
+    //список покупців, із вказаним іменем
     public List<Customer> searchName(List<Customer> list, String name) {
         return list.stream().filter(customer -> customer.getFullName().equals(name))
                 .collect(Collectors.toList());
-
     }
 
+    //список покупців, у яких номер кредитної картки знаходиться в заданому інтервалі
     public List<Customer> searchCredCard(List<Customer> list, long start, long end) {
         return list.stream().filter(customer -> customer.getCreditCardNum() >= start
                 && customer.getCreditCardNum() <= end)
                 .collect(Collectors.toList());
     }
 
+    // кількість та список покупців, які мають заборгованість (від’ємний баланс на карті)
+    // в порядку зростання заборгованості
     public List<Customer> searchDebt(List<Customer> list) {
         //Comparator<Customer> comparator = Comparator.comparingDouble(c -> -c.getBalance());
         return list.stream().filter(customer -> customer.getBalance() < 0)
@@ -34,10 +46,19 @@ public class ListWorker {
                 .collect(Collectors.toList());
     }
 
-    public List<Customer> listSort(List<Customer> list) {
-        list.sort(Comparator.comparingDouble(Customer::getBalance)
-                .thenComparing(Customer::getCreditCardNum));
-        return list;
+    //список років народження покупців, зареєстрованих у програмі без повторів
+    public Set<LocalDate> setDates(List<Customer> list) {
+        return list.stream()
+                .map(Customer::getBirthday)
+                .collect(Collectors.toSet());
     }
 
+    //для кожного року народження визначити покупця, що має найбільшу кількість грошей на картці
+    public Map<Integer, Customer> richByYear(List<Customer> list) {
+        Comparator<Customer> bal = Comparator.comparingDouble(Customer::getBalance);
+        return list.stream()
+                .collect(Collectors.groupingBy(Customer::getYear,
+                        Collectors.reducing(null,
+                                BinaryOperator.maxBy(Comparator.nullsFirst(bal)))));
+    }
 }
